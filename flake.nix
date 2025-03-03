e2fc5eb3a305e7eedcf14ec41e20b56eb56a77c8
Will Leinweber <will@bitfission.com>
2025-03-03 14:22:16 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ]
+        (system: function nixpkgs.legacyPackages.${system});
+  in {
+    # packages = forAllSystems (pkgs: {
+    #   default = pkgs.callPackage ./package.nix {};
+    # });
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell { packages = [ pkgs.ruby ];};
+    });
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {};
+  };
+}
