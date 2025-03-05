a8ae57feb38a8521b56fe0a986c1188385f946a2
Will Leinweber <will@bitfission.com>
2025-03-05 11:42:46 +0100

    use overlay insetad of addGems function

--- flake.nix
+++ flake.nix
@@ -2,22 +2,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-  outputs = { nixpkgs, ... }:
-    let
-      addGems = ruby: ruby.withPackages (ps: with ps; [ sinatra rackup ]);
 
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs
-          [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ]
-          (system: function nixpkgs.legacyPackages.${system});
+  outputs = { self, nixpkgs, ... }:
+    let
+      overlays = [ (final: prev: { rubyWithGems = prev.ruby.withPackages (ps: with ps; [ sinatra rackup ]); }) ];
+      forAllSystems = function: nixpkgs.lib.genAttrs
+        [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ]
+        (system: function (import nixpkgs { inherit system overlays; }));
     in
     rec {
       packages = forAllSystems (pkgs: {
-        default = pkgs.writeScriptBin "app" ("#!${addGems pkgs.ruby}/bin/ruby\n" + builtins.readFile ./app.rb);
+        default = pkgs.writeScriptBin "app" ("#!${pkgs.rubyWithGems}/bin/ruby\n" + builtins.readFile ./app.rb);
       });
 
       devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell { packages = with pkgs; [ (addGems ruby) nixpkgs-fmt ]; };
+        default = pkgs.mkShell { packages = with pkgs; [ rubyWithGems nixpkgs-fmt ]; };
       });
 
       nixosConfigurations.container = nixpkgs.lib.nixosSystem {
@@ -25,6 +24,7 @@
           {
             nixpkgs.hostPlatform = "aarch64-linux";
             boot.isContainer = true;
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
             networking.nftables.enable = true;
             networking.firewall.allowedTCPPorts = [ 80 ];
           }
@@ -41,7 +41,7 @@
                   Environment = [ "PORT=80" "APP_ENV=production" ];
                   KillMode = "mixed";
                   Restart = "always";
-                  RuntimeDirectory = "hawk";
+                  RuntimeDirectory = "app";
                 };
               };
             })
